aed39b1676b08a3ca8d6f9110318a6456c4a1370
Anmol Kaur <kaur.anmol26@gmail.com>
2020-05-14 21:12:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+ 
 end
 
 def words_five_letters_long(text)
